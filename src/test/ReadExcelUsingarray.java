43bd15f144cda0910f994a0013970bc1a27c7800
Vishal1304 <vishal13k.s@gmail.com>
2018-09-11 22:31:27 +0530

    data driven code

---
+++ src/test/ReadExcelUsingarray.java
@@ -0,0 +1,93 @@
+package test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class ReadExcelUsingarray {
+	public static WebDriver driver;
+	public static String FilePath = "G:\\TestData.xlsx";
+	public static XSSFSheet SheetName;
+	public static XSSFWorkbook workbook;
+	public static XSSFSheet sheet;
+	public static FileInputStream fis;
+	public static Object[][] data;
+
+	@Test(dataProvider = "getdata")
+	public void loadtest(String Username, String Password) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "G:\\java_workspace\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		// Launch the URL
+		Thread.sleep(5000);
+		driver.get("http://demo.guru99.com/V4/");
+		// Find user name
+		Thread.sleep(2000);
+		WebElement userName = driver.findElement(By.name("uid"));
+		// Fill user name
+		userName.sendKeys(Username);
+		// Find password
+		WebElement password = driver.findElement(By.name("password"));
+		// Fill password
+		password.sendKeys(Password);
+		Thread.sleep(2000);
+		driver.close();
+	}
+
+	@DataProvider
+	public Object[][] getdata() throws Exception {
+
+		try {
+			File file = new File(FilePath);
+			 fis = new FileInputStream(file);
+			workbook = new XSSFWorkbook(fis);
+			SheetName = workbook.getSheet("Sheet1");
+			int rowcount = SheetName.getLastRowNum();
+			int cellcount = SheetName.getRow(rowcount).getLastCellNum();
+			 data = new Object[rowcount + 1][cellcount];
+
+			for (int i = 0; i < rowcount + 1; i++) {
+				for (int j = 0; j < cellcount; j++) {
+					XSSFRow row = SheetName.getRow(i);
+
+					XSSFCell uname = row.getCell(j);
+					data[i][j] = uname.getStringCellValue();
+
+					/*
+					 * XSSFCell pwd= row.getCell(1); data[i][1]=pwd.getStringCellValue();
+					 */
+
+				}
+			}
+			
+			
+		} 
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		finally {
+			fis.close();
+		}
+		return data;
+		
+
+	}
+}
